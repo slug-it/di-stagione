a52a89ecdcab7754d4aa190efc1303945de81ff9
Matteo Bertini <naufraghi@develer.com>
2013-10-07 17:28:50 +0200

    Add test SListView and SArrayAdapter

--- src/it/slug/distagione/ListaDiStagione.scala
+++ src/it/slug/distagione/ListaDiStagione.scala
@@ -7,6 +7,7 @@ class ListaDiStagione extends SActivity {
   onCreate {
     contentView = new SVerticalLayout {
       STextView("Hello from Scaloid")
+      SListView().setAdapter(SArrayAdapter("Primo", "Secondo", "Terzo"))
     }
   }
 }
