c11de43654c74e8becf6dc21c8f3099f44ee6209
Matteo Bertini <naufraghi@develer.com>
2013-10-07 16:52:22 +0200

    Use scaloid compact style

--- src/it/slug/distagione/ListaDiStagione.scala
+++ src/it/slug/distagione/ListaDiStagione.scala
@@ -4,9 +4,7 @@ import android.os.Bundle
 import org.scaloid.common._
 
 class ListaDiStagione extends SActivity {
-  override def onCreate(savedInstanceState: Bundle) {
-    super.onCreate(savedInstanceState)
-
+  onCreate {
     contentView = new SVerticalLayout {
       STextView("Hello from Scaloid")
     }
