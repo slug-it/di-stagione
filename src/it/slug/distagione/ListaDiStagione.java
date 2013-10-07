0a4ea294aa30035f332518712a006fe1a4e37de7
Matteo Bertini <naufraghi@develer.com>
2013-10-07 10:59:08 +0200

    Initial commit: android create project -n "DiStagione" -t android-8 -p DiStagione -k it.slug.distagione -a ListaDiStagione

---
+++ src/it/slug/distagione/ListaDiStagione.java
@@ -0,0 +1,15 @@
+package it.slug.distagione;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListaDiStagione extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
