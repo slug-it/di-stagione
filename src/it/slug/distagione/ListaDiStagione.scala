798c28ab4ae55752094e7f75b43400f95a4937aa
Matteo Bertini <naufraghi@develer.com>
2013-10-09 22:58:52 +0200

    Add some style to list elements

--- src/it/slug/distagione/ListaDiStagione.scala
+++ src/it/slug/distagione/ListaDiStagione.scala
@@ -1,5 +1,6 @@
 package it.slug.distagione
 
+import android.graphics.Color
 import android.os.Bundle
 import org.scaloid.common._
 
@@ -7,7 +8,8 @@ class ListaDiStagione extends SActivity {
   onCreate {
     contentView = new SVerticalLayout {
       STextView("Hello from Scaloid")
-      SListView().setAdapter(SArrayAdapter("Primo", "Secondo", "Terzo"))
+      val produces = SArrayAdapter("Primo", "Secondo", "Terzo").style(_.textColor(Color.YELLOW))
+      SListView().setAdapter(produces)
     }
   }
 }
