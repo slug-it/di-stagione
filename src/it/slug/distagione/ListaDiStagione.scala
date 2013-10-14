b05eb107d612a3ee26a7287f5b1102ef79bc29ab
Matteo Bertini <naufraghi@develer.com>
2013-10-14 15:50:28 +0200

    Add custom fonts to the assets and use the new typefaces

--- src/it/slug/distagione/ListaDiStagione.scala
+++ src/it/slug/distagione/ListaDiStagione.scala
@@ -1,14 +1,26 @@
 package it.slug.distagione
 
-import android.graphics.Color
 import android.os.Bundle
+import android.graphics.Color
+import android.graphics.Typeface
+
 import org.scaloid.common._
+
+object ListaDiStagione {
+  var tf_title: Typeface = null
+  var tf_element: Typeface = null
+}
 
 class ListaDiStagione extends SActivity {
   onCreate {
+    // FIXME: do this one time setup in the constructor
+    if (ListaDiStagione.tf_title == null) {
+      ListaDiStagione.tf_title = Typeface.createFromAsset(getAssets(), "fonts/OverlockSC-Regular.ttf")
+      ListaDiStagione.tf_element = Typeface.createFromAsset(getAssets(), "fonts/SortsMillGoudy-Regular.ttf")
+    }
     contentView = new SVerticalLayout {
-      STextView("Hello from Scaloid")
-      val produces = SArrayAdapter("Primo", "Secondo", "Terzo").style(_.textColor(Color.YELLOW))
+      STextView("Hello from Scaloid").setTypeface(ListaDiStagione.tf_title)
+      val produces = SArrayAdapter("Primo", "Secondo", "Terzo").style(_.textColor(Color.YELLOW).typeface(ListaDiStagione.tf_element))
       SListView().setAdapter(produces)
     }
   }
