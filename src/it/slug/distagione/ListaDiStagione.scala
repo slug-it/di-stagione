7d2deaeef0f464e82cb2623fa82ca8df512a9baf
gatsu <ag@gatsu-laptop.(none)>
2013-11-09 18:58:31 +0100

    Aggiunta activity per il dettaglio del singolo mese. Per adesso l'activity è vuota e raggiungibile cliccando su un menu item temporaneo nella main activity.

--- src/it/slug/distagione/ListaDiStagione.scala
+++ src/it/slug/distagione/ListaDiStagione.scala
@@ -5,9 +5,12 @@ import android.os.Bundle
 import android.graphics.Color
 import android.graphics.Typeface
 import android.widget.Button
-
-// customized button
 import android.content.Context
+import android.content.Intent
+import android.view.Menu
+import android.view.View
+import android.view.MenuItem
+import android.util.Log
 import android.util.AttributeSet
 
 
@@ -20,6 +23,23 @@ class ListaDiStagione extends Activity with TypedViewHolder {
     super.onCreate(saved)
     setContentView(R.layout.main)
     findView(TR.text).setTypeface(tf_title)
+  }
+
+  override def onCreateOptionsMenu(menu: Menu) : Boolean = {
+    getMenuInflater().inflate(R.layout.menu_main, menu)
+    return true
+  }
+
+  override def onOptionsItemSelected(item: MenuItem) : Boolean = {
+    item.getItemId() match {
+      case R.id.test_detail_menu => {
+        val intent = new Intent(ListaDiStagione.this, 
+                                classOf[MonthDetailActivity])
+        startActivity(intent)
+        return true
+      }
+      case _ => return super.onOptionsItemSelected(item)
+    }
   }
 }
 
---
+++ src/it/slug/distagione/MonthDetailActivity.scala
@@ -0,0 +1,13 @@
+package it.slug.distagione;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+class MonthDetailActivity extends Activity {
+
+    override def onCreate(savedInstanceState: Bundle) : Unit = {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.month_detail);
+    }
+}
