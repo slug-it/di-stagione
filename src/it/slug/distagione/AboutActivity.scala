a47aa9138f35cf8dbbfdddb8621fe574e52388c5
gatsu <ag@gatsu-laptop.(none)>
2014-03-12 22:27:25 +0100

    about activity: app name string is now fetched from resources

--- src/it/slug/distagione/AboutActivity.scala
+++ src/it/slug/distagione/AboutActivity.scala
@@ -13,8 +13,6 @@ class AboutActivity extends Activity with TypedViewHolder {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.about)
 
-        findView(TR.about_header).setText("Di Stagione")
-
         val pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
         val version = pInfo.versionName;
         findView(TR.about_ver).setText("v" + version)
