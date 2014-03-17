06ad60b943b5bef64340987f32c1126be682c61e
gatsu <ag@gatsu-laptop.(none)>
2014-03-17 18:59:08 +0100

    added data links to about activity

--- src/it/slug/distagione/AboutActivity.scala
+++ src/it/slug/distagione/AboutActivity.scala
@@ -19,14 +19,25 @@ class AboutActivity extends Activity with TypedViewHolder {
 
         val imgCredits = findView(TR.about_img_credits)
         imgCredits.setText(Html.fromHtml(
-            "icon by <a href=\"http://mike44nh.deviantart.com/\">mike44nh</a>" +
+            "icona by <a href=\"http://mike44nh.deviantart.com/\">mike44nh</a>" +
             " (by-nc-sa 3.0)"))
         imgCredits.setMovementMethod(LinkMovementMethod.getInstance())
+
+        val dataLinks = findView(TR.about_data_links)
+        dataLinks.setText(Html.fromHtml(
+            "<br />Dati frutta/verdura:<br />" +
+            "<a href=\"http://www.paginainizio.com/service/verdurastagione.htm"+
+            "\">paginainizio.com</a><br />" +
+            "<a href=\"http://www.verduredistagione.it/\">" +
+            "verduredistagione.it</a><br />" +
+            "<a href=\"http://www.unaproa.com\">unaproa.com</a><br />"
+            ))
+        dataLinks.setMovementMethod(LinkMovementMethod.getInstance())
 
         val githubrepo = findView(TR.about_repo)
         githubrepo.setText(Html.fromHtml(
             "github <a href=\"https://github.com/slug-it/di-stagione\">" +
-            "app repo</a>"
+            "repository</a>"
             ))
         githubrepo.setMovementMethod(LinkMovementMethod.getInstance())
     }
